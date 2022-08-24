6a1a1650b0c9229f25d75f46034fe0386959f5a5
latha <latha@LAPTOP-FS8UA1PI>
2022-08-24 22:46:28 +0530

    commit

---
+++ JavaDemos/src/com/tnsif/lesson2/Zero.java
@@ -0,0 +1,11 @@
+package com.tnsif.lesson2;
+
+public class Zero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
